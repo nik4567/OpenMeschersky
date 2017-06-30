e3a650bcbdfc6d0c1c8b6cca226cd887f45bad55
nickkolok <nickkolok@mail.ru>
2017-07-01 00:05:35 +0300

    Скриптик проверки пуллреквеста рапортует о том, что фетчит

--- check-task.sh
+++ check-task.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 for rem in `git remote`; do
+	echo "Fetching remote $rem..."
 	git fetch $rem
 done;
+echo 'Remotes fetched'
 git checkout $1
 git rebase -i origin/master
 make UNOCONV=echo PDFCROP=echo
